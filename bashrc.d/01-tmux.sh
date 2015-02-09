f9543d40e4677624ddbf80eec8d1112881d99f71
Hugh Davenport <hugh@davenport.net.nz>
2015-02-09 15:53:26 +1300

    bash: Start tmux automatically

---
+++ bashrc.d/01-tmux.sh
@@ -0,0 +1,7 @@
+source /usr/share/doc/tmux/examples/bash_completion_tmux.sh
+case $- in *i*)
+    if [ -z "$TMUX" ]; then
+        # Would be really cool to choose an existing session and start that
+        exec tmux -2
+    fi
+esac
