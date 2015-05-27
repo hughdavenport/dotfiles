80e229d474869c95a4da8f9b641a7ba0bea7b654
Hugh Davenport <hugh@davenport.net.nz>
2015-05-27 13:06:41 +1200

    bashrc.d: Add dynamic colours to urxvt
    Requires some .Xresources and a git clone, will add later

---
+++ bashrc.d/04-urxvt-dynamic-colors.sh
@@ -0,0 +1,9 @@
+if [ -z "$DISPLAY" -o -n "$SSH_CONNECTION" ]; then
+    return;
+fi
+export PATH="$HOME/.dynamic-colors/bin:$PATH"
+X_ID=$(xdpyinfo | grep focus | cut -f4 -d " " | sed -e 's/,$//')
+export DYNAMIC_COLORS_SUFFIX=".${X_ID}"
+source $HOME/.dynamic-colors/completions/dynamic-colors.bash
+dynamic-colors switch solarized-dark
+dynamic-colors init
