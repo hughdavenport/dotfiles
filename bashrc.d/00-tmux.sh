da6b9b19d5711ad8fe301abb52a91081d36a60a2
Hugh Davenport <hugh@davenport.net.nz>
2015-05-28 08:39:24 +1200

    tmux: Add script to choose which session on start
    Can hook it into window manager, or just through bash (SSH). Shows a
    list of sessions, and you choose from them, or leave blank to get
    default autonumbered session, or type in a new session name.

--- bashrc.d/00-tmux.sh
+++ bashrc.d/00-tmux.sh
@@ -3,7 +3,6 @@ tmux -V >/dev/null 2>&1 || return
 source /usr/share/doc/tmux/examples/bash_completion_tmux.sh
 case $- in *i*)
     if [ -z "$TMUX" ]; then
-        # Would be really cool to choose an existing session and start that
-        exec tmux -2
+	exec ~/bin/tmux-chooser
     fi
 esac
