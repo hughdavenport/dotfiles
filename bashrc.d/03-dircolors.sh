657a89d8638c4e923fc1174d7743aa889d9db0ee
Hugh Davenport <hugh@davenport.net.nz>
2015-05-27 14:16:24 +1200

    bash: Add aliases for dircolors

--- bashrc.d/03-dircolors.sh
+++ bashrc.d/03-dircolors.sh
@@ -1,1 +1,7 @@
 eval `dircolors $HOME/.dircolors-solarized/dircolors.ansi-universal`
+alias ls='ls --color=auto'
+alias dir='dir --color=auto'
+alias vdir='dir --color=auto'
+alias grep='grep --color=auto'
+alias fgrep='fgrep --color=auto'
+alias egrep='egrep --color=auto'
