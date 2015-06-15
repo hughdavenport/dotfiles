b75857f3ccb9363be2c507e0b5535aff612b369d
Hugh Davenport <hugh@davenport.net.nz>
2015-06-15 14:03:25 +1200

    tmux: Don't get in endless loop falling back to straight bash

--- bashrc.d/00-tmux.sh
+++ bashrc.d/00-tmux.sh
@@ -1,5 +1,7 @@
+[ -v TMUX_FALLBACK ] && return
 which tmux >/dev/null || return
 tmux -V >/dev/null 2>&1 || return
+test -x ~/bin/tmux-chooser || return
 source /usr/share/doc/tmux/examples/bash_completion_tmux.sh
 case $- in *i*)
     if [ -z "$TMUX" ]; then
