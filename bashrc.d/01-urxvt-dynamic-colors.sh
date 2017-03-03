3372c6ffa85db510850af2cc0ed1d93d2941e87f
Hugh Davenport <hugh@davenport.net.nz>
2017-03-04 08:47:02 +1300

    Dynamic colors needs the link

--- bashrc.d/01-urxvt-dynamic-colors.sh
+++ bashrc.d/01-urxvt-dynamic-colors.sh
@@ -1,9 +1,9 @@
 if [ -z "$DISPLAY" -o -n "$SSH_CONNECTION" -o -n "$DYNAMIC_COLORS_SUFFIX" ]; then
     return;
 fi
-export PATH="$HOME/.dotfiles/submodules/dynamic-colors/bin:$PATH"
+export PATH="$HOME/.dynamic-colors/bin:$PATH"
 X_ID=$(xdpyinfo | grep focus | cut -f4 -d " " | sed -e 's/,$//')
 export DYNAMIC_COLORS_SUFFIX=".${X_ID}"
-source $HOME/.dotfiles/submodules/dynamic-colors/completions/dynamic-colors.bash
+source $HOME/.dynamic-colors/completions/dynamic-colors.bash
 dynamic-colors switch solarized-dark
 dynamic-colors init
