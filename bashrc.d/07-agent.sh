202756948a843110bc59b643572dd9474b042baa
Hugh Davenport <hugh@davenport.net.nz>
2017-03-13 20:52:28 +1300

    Now using ssh-ident so don't find an old agent

--- bashrc.d/07-agent.sh
+++ bashrc.d/07-agent.sh
@@ -1,4 +1,3 @@
-[ -z "$SSH_AUTH_SOCK" ] && export SSH_AUTH_SOCK=`ls /tmp/ssh-*/agent.*`
 if [ -f "${HOME}/.gpg-agent-info" ]; then
   . "${HOME}/.gpg-agent-info"
   export GPG_AGENT_INFO
@@ -6,6 +5,7 @@ fi
 echo | gpg-connect-agent 2>/dev/null || \
     eval $(gpg-agent --daemon --write-env-file "${HOME}/.gpg-agent-info")
 export GPG_TTY=$(tty)
+# env and aliases for ssh-ident
 export BINARY_SSH="~/bin/ssh-tmux-helper"
 alias scp="BINARY_SSH=scp ssh"
 alias sftp="BINARY_SSH=sftp ssh"
