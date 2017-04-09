754c8be810d1b781e5357c9a0a3da457404ee5be
Hugh Davenport <hugh@davenport.net.nz>
2017-04-09 19:04:45 +1200

    Autostart the printer applet

--- bin/i3wm-startup.sh
+++ bin/i3wm-startup.sh
@@ -6,5 +6,8 @@ xrdb -merge ~/.Xresources
 # Minimize wpa_gui to tray
 wpa_gui -t &
 
+# Start printer applet
+system-config-printer-applet &
+
 # Start xautolock
 xautolock -corners +-00 -cornerredelay 60 -time 1 &
