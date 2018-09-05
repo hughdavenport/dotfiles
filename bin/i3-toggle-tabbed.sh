d8879742a27b99eada2dc969ab4768e4c0c8c1a9
Hugh Davenport <hugh@davenport.net.nz>
2018-09-05 12:37:46 +1200

    missing stuff

--- bin/i3-toggle-tabbed.sh
+++ bin/i3-toggle-tabbed.sh
@@ -4,10 +4,10 @@ if ! `which jq >/dev/null 2>&1`; then
      echo "Need to install jq"
      exit 1
 fi
-layout=$(i3-save-tree | grep -v '\/\/' | jq .nodes[0].layout | sed -e 's/^"//;s/"$//')
+layout=$(i3-save-tree | grep -v '\/\/' | jq .nodes[].layout | grep -v null | head -1 | sed -e 's/^"//;s/"$//')
 echo $layout
 
-if [ "$layout" == "null" ]; then
+if [ -z "$layout" ]; then
      layout=$(i3-save-tree | grep -v '\/\/' | jq .layout | sed -e 's/^"//;s/"$//')
      echo $layout
 fi
