b694e6b0404e0bbb67df321cc9b5484aa8880ce4
Hugh Davenport <hugh@davenport.net.nz>
2020-05-20 14:11:11 +1200

    [bash] Add uridecode as well

--- bashrc.d/99-uriencode.sh
+++ bashrc.d/99-uriencode.sh
@@ -2,3 +2,7 @@
 function uriencode_arg { jq -nr --arg v "$1" '$v|@uri'; }
 function uriencode_post { jq -sRr '@uri'; }
 function uriencode { if [ -z "$1" ]; then uriencode_post; else uriencode_arg "$1"; fi }
+
+# Adapted from https://stackoverflow.com/a/37840948
+function uridecode_arg { : "${*//+/ }"; echo -e "${_//%/\\x}"; }
+function uridecode { if [ -z "$1" ]; then uridecode_arg "$(cat)"; else uridecode_arg "$1"; fi }
